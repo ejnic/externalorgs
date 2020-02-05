3df072d7a8baa617bdf244458f3e8f8b2c2e912c
ejnic <ejnicodemus@gmail.com>
2020-02-05 12:09:25 -0500

    updates for loading final matches to oracle

--- matched.py
+++ matched.py
@@ -4,28 +4,18 @@ import oraclecon as con
 
 
 df = pd.read_excel('C:\\Users\\ejnic\\Google Drive Personal\\Python\\externalorgs\\files\\OrgIDMatchesforLiaison.xlsx',
-                   skiprows=1, usecols = "A:G", sheet_name='Sheet1',
-                   names = ['keep', 'matchratio', 'nameL', 'nameIU', 'cntry', 'liaison', 'iu'])
-
-
+                   skiprows=1, usecols = "B:H", sheet_name='Sheet1',
+                   names = ['keep', 'matchratio', 'nameL', 'nameIU', 'cntry', 'liaison', 'iu'],
+                   converters={'iu':str})
+df.keep.fillna('Yes', inplace=True)
+df = df[(df.keep == 'Yes')]
 
 engine = sqlalchemy.create_engine(con.strengine)
 
 df.to_sql('extorgsmatched', engine, con.schema, if_exists='replace', chunksize=1000,
                  dtype=
-                 {'campus':sqlalchemy.types.VARCHAR(df.campus.str.len().max()),
-                 'first_name':sqlalchemy.types.VARCHAR(df.first_name.str.len().max()),
-                 'last_name':sqlalchemy.types.VARCHAR(df.last_name.str.len().max()),
-                 'email':sqlalchemy.types.VARCHAR(df.email.str.len().max()),
-                 'programname':sqlalchemy.types.VARCHAR(df.programname.str.len().max()),
-                 'webadmitname':sqlalchemy.types.VARCHAR(df.webadmitname.str.len().max()),
-                 'roles':sqlalchemy.types.VARCHAR(df.roles.str.len().max()),
-                 'networkid':sqlalchemy.types.VARCHAR(25),
-                 'is_active':sqlalchemy.types.VARCHAR(2),
-                 'users_created_at':sqlalchemy.types.VARCHAR(50),
-                 'users_created_at2':sqlalchemy.types.VARCHAR(50),
-                 'last_login_at':sqlalchemy.types.VARCHAR(50),
-                 'login_count':sqlalchemy.types.VARCHAR(25)
+                 {'liaison':sqlalchemy.types.VARCHAR(df.liaison.str.len().max()),
+                 'iu':sqlalchemy.types.VARCHAR(df.iu.str.len().max())
                  })
 
 print(df.head(10))
